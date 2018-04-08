570ca9326e29fcb966f3c6fa7edac8f006e1410b
marcusbilgec <marcusbilgec@live.se>
2018-04-08 20:02:51 +0200

    Testfolder added, facialexprecionn made and implemented with switch

--- sweeperRobot/sweeperRobot.ino
+++ sweeperRobot/sweeperRobot.ino
@@ -3,20 +3,22 @@
 #include "Adafruit_LEDBackpack.h"
 #include <Servo.h>
 
-Adafruit_8x8matrix sweeperEye1 = Adafruit_8x8matrix();
-#define irSensorInputPin 7
-#define servoLeftPin 9
-#define servoRightPin 10
+Adafruit_8x8matrix sweeperEyeLeft = Adafruit_8x8matrix();
+Adafruit_8x8matrix sweeperEyeRight = Adafruit_8x8matrix();
+#define bumperSwitch 7
+#define servoLeftPin 5
+#define servoRightPin 6
 Servo sweeperLeftServo;
 Servo sweeperRightServo;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  sweeperEye1.begin(0x70);
-  pinMode(irSensorInputPin,INPUT);
-  sweeperLeftServo.attach(9);
-  sweeperRightServo.attach(10);
+  sweeperEyeLeft.begin(0x70);
+  sweeperEyeRight.begin(0x70);
+  pinMode(bumperSwitch,INPUT);
+  sweeperLeftServo.attach(5);
+  sweeperRightServo.attach(6);
 
   //Counter clockwise
   sweeperLeftServo.writeMicroseconds(1550);
@@ -41,34 +43,50 @@ static const uint8_t PROGMEM
     B10000001,
     B01000010,
     B00111100 },
-  frown_bmp[] =
-  { B00111100,
-    B01000010,
-    B10100101,
-    B10000001,
-    B10011001,
-    B10100101,
-    B01000010,
-    B00111100 };
+  happy_bmp[] =
+  { B00001110,
+    B00011100,
+    B00111000,
+    B01110000,
+    B01110000,
+    B00111000,
+    B00011100,
+    B00001110 },
+  angry_bmp[] =
+  { B00011110,
+    B00011110,
+    B00111100,
+    B00111100,
+    B01111000,
+    B01111000,
+    B11110000,
+    B11110000 };
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sweeperEye1.clear();
-  if(digitalRead(irSensorInputPin))
+  sweeperEyeLeft.clear();
+  if(digitalRead(bumperSwitch))
   {
-    sweeperEye1.drawBitmap(0,0,smile_bmp,8,8,LED_ON);
-    sweeperEye1.writeDisplay();
-    // delay(1000);
+    sweeperEyeLeft.drawBitmap(0,0,angry_bmp,8,8,LED_ON);
+    sweeperEyeLeft.writeDisplay();
+    sweeperEyeRight.drawBitmap(0,0,angry_bmp,8,8,LED_ON);
+    sweeperEyeRight.writeDisplay();
+    //delay(1000);
   }
-  else if(!digitalRead(irSensorInputPin))
+  else if(!digitalRead(bumperSwitch))
   {
-    sweeperEye1.drawBitmap(0,0,frown_bmp,8,8,LED_ON);
-    sweeperEye1.writeDisplay();
-    // delay(1000);
+    sweeperEyeLeft.drawBitmap(0,0,happy_bmp,8,8,LED_ON);
+    sweeperEyeLeft.writeDisplay();
+    sweeperEyeRight.drawBitmap(0,0,happy_bmp,8,8,LED_ON);
+    sweeperEyeRight.writeDisplay();
+
+    
+   
+   // delay(1000);
   }
   else
   {
-    sweeperEye1.clear();
-    sweeperEye1.writeDisplay();
+    sweeperEyeLeft.clear();
+    sweeperEyeLeft.writeDisplay();
   }
 }
